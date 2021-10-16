571ab59ea9d6c1bb75f5f11557308e9518af6897
kisky3 <kisky3@126.com>
2021-10-16 23:35:35 +0900

    :+1: create Vue Project

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <AdviceMessage />
+    <TodoContainer></TodoContainer>
+  </div>
+</template>
+
+<script>
+import AdviceMessage from './components/AdviceMessage'
+import TodoContainer from './components/TodoContainer.vue'
+
+export default {
+  name: 'App',
+  components: {
+    TodoContainer,
+    AdviceMessage,
+  },
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
